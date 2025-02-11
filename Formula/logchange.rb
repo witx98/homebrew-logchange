2717b247e54c12b35464f2a5809c031dc84ed387
witx98 <witx90@gmail.com>
2025-02-11 21:56:52 +0100

    Update formula

--- Formula/logchange.rb
+++ Formula/logchange.rb
@@ -4,32 +4,32 @@ class Logchange < Formula
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    suffix "linuxx64"
-    url "https://github.com/logchange/logchange/releases/download/1.16.7/logchange-#{suffix}.zip"
+    url "https://github.com/logchange/logchange/releases/download/1.16.7/logchange-linuxx64.zip"
     sha256 "409df1a137ee2461ec8636d2377e64478df6fa3b53bc85c942ce7bcf3852a031"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    suffix "darwinarm64"
-    url "https://github.com/logchange/logchange/releases/download/1.16.7/logchange-#{suffix}.zip"
-    sha256 "5f6583fb29c025806d6d5496cec360f3364c40ddb7764f7cb1f0967d1275b4e0"
-  end
-  if OS.mac? && Hardware::CPU.intel?
-    suffix "darwinx64"
-    url "https://github.com/logchange/logchange/releases/download/1.16.7/logchange-#{suffix}.zip"
-    sha256 "7cc67b0b4f7b90098c78e163bce03825f2edcc76e85fcb3586e951754a7265ad"
-  end
-
-  def install
-    libexec.install Dir["*"]
-    if OS.linux? && Hardware::CPU.intel?
+    def install
+      libexec.install Dir["*"]
       chmod 0755, "#{libexec}/logchange-linuxx64/logchange"
       bin.write_exec_script "#{libexec}/logchange-linuxx64/logchange"
-    elsif OS.mac? && Hardware::CPU.arm?
+    end
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/logchange/logchange/releases/download/1.16.7/logchange-darwinarm64.zip"
+    sha256 "9a0cdd9714f18777a17b233c2acfbfd32c0a6ef99a0730a81576668cc559fd3b"
+    def install
+      libexec.install Dir["*"]
       chmod 0755, "#{libexec}/logchange-darwinarm64/logchange"
-      bin.write_exec_script "#{libexec}/logchange-darwinx64/logchange"
-    elsif OS.mac? && Hardware::CPU.intel?
-      chmod 0755, "#{libexec}/logchange-darwinx64/logchange"
       bin.write_exec_script "#{libexec}/logchange-darwinarm64/logchange"
+    end
+  end
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/logchange/logchange/releases/download/1.16.7/logchange-darwinx64.zip"
+    sha256 "f363c98d6387aaaa1c4e8ddce0813e1214a13515218e0fa7f5eca3c0a2cbefc8"
+    def install
+      libexec.install Dir["*"]
+      chmod 0755, "#{libexec}/logchange-darwinx64/logchange"
+      bin.write_exec_script "#{libexec}/logchange-darwinx64/logchange"
     end
   end
 end
